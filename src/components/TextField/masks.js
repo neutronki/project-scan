791b770cddc0edfc0636ca6ad3dc3680e2a00b3a
neutronki <87097661+neutronki@users.noreply.github.com>
2023-09-25 02:04:47 +0300

    Create masks.js

---
+++ src/components/TextField/masks.js
@@ -0,0 +1,27 @@
+const inn10Mask = [
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+  /\d/,
+  ' ',
+  /\d/,
+  /\d/,
+];
+
+const getMaskByInputName = ({ name }) => {
+  switch (name) {
+    case 'inn':
+      return inn10Mask;
+
+    default:
+      return false;
+  }
+};
+
+export default getMaskByInputName;
